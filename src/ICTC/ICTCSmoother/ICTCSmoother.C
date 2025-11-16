db7b1d3095e4aecdd4f8f0f40dda954506050f4c
Davide <davidechiesa29@gmail.com>
2025-11-16 14:36:35 -0600

    Updated ICTC folder structure and created paramaterized ICTCSmoother

---
+++ src/ICTC/ICTCSmoother/ICTCSmoother.C
@@ -0,0 +1,59 @@
+/*---------------------------------------------------------------------------*\
+\*---------------------------------------------------------------------------*/
+
+#include "ICTCSmootherBase.H"
+#include "ICTCSmoother.H"
+#include "PrecisionAdaptor.H"
+#include <algorithm>
+#include <cmath>
+
+#define MAKE_PARAMATERIZED_ICTC_SMOOTHER(ClassName, dropTol)                   \
+namespace Foam                                                                 \
+{                                                                              \
+    defineTypeNameAndDebug(ClassName, 0);                                      \
+                                                                               \
+    lduMatrix::smoother::                                                      \
+    addsymMatrixConstructorToTable<ClassName>                                  \
+        add##ClassName##SymMatrixConstructorToTable_;                          \
+}                                                                              \
+                                                                               \
+Foam::ClassName::ClassName                                                     \
+(                                                                              \
+    const word& fieldName,                                                     \
+    const lduMatrix& matrix,                                                   \
+    const FieldField<Field, scalar>& interfaceBouCoeffs,                       \
+    const FieldField<Field, scalar>& interfaceIntCoeffs,                       \
+    const lduInterfaceFieldPtrsList& interfaces,                               \
+    const dictionary& dict                                                     \
+)                                                                              \
+:                                                                              \
+    ICTCSmootherBase                                                           \
+    (                                                                          \
+        fieldName,                                                             \
+        matrix,                                                                \
+        interfaceBouCoeffs,                                                    \
+        interfaceIntCoeffs,                                                    \
+        interfaces,                                                            \
+        (dropTol)                                                              \
+    )                                                                          \
+{}                                                                             \
+
+
+// Instantiates ICTCSmoother Variants
+
+MAKE_PARAMATERIZED_ICTC_SMOOTHER(ICTC_m4,    1e-4);
+MAKE_PARAMATERIZED_ICTC_SMOOTHER(ICTC_m3p5,  std::pow(10.0, -3.5));
+MAKE_PARAMATERIZED_ICTC_SMOOTHER(ICTC_m3,    1e-3);
+MAKE_PARAMATERIZED_ICTC_SMOOTHER(ICTC_m2p5,  std::pow(10.0, -2.5));
+MAKE_PARAMATERIZED_ICTC_SMOOTHER(ICTC_m2,    1e-2);
+MAKE_PARAMATERIZED_ICTC_SMOOTHER(ICTC_m1p5,  std::pow(10.0, -1.5));
+MAKE_PARAMATERIZED_ICTC_SMOOTHER(ICTC_m1,    1e-1);
+MAKE_PARAMATERIZED_ICTC_SMOOTHER(ICTC_m0p5,  std::pow(10.0, -0.5));
+
+// Default Smoother Instantiation
+MAKE_PARAMATERIZED_ICTC_SMOOTHER(ICTC,  1);
+
+#undef MAKE_PARAMATERIZED_ICTC_SMOOTHER
+
+// ************************************************************************* //
+
---
+++ src/ICTC/ICTCSmoother/ICTCSmootherBase.C
@@ -0,0 +1,120 @@
+/*---------------------------------------------------------------------------*\
+
+\*---------------------------------------------------------------------------*/
+
+#include "ICTCSmootherBase.H"
+#include "../ICTCPreconditioner/ICTCPreconditioner.H"
+#include "PrecisionAdaptor.H"
+#include <algorithm>
+
+
+// * * * * * * * * * * * * * * * * Constructors  * * * * * * * * * * * * * * //
+
+Foam::ICTCSmootherBase::ICTCSmootherBase
+(
+     const word& fieldName,
+     const lduMatrix& matrix,
+     const FieldField<Field, scalar>& interfaceBouCoeffs,
+     const FieldField<Field, scalar>& interfaceIntCoeffs,
+     const lduInterfaceFieldPtrsList& interfaces,
+     const scalar droptol
+)
+:
+    lduMatrix::smoother
+    (
+     	fieldName, 
+	matrix, 
+	interfaceBouCoeffs, 
+	interfaceIntCoeffs, 
+	interfaces
+    ),
+    
+    diagL_(matrix_.diag().size()),
+    lowerL_(200 * matrix_.diag().size()),
+    rowAddrL_(200 * matrix_.diag().size()),
+    colPtrL_(matrix_.diag().size() + 1)
+    
+
+{
+    const scalar pivmin = 1e-16;
+    ICTCPreconditioner::calcL(diagL_, lowerL_, rowAddrL_, colPtrL_, matrix_, droptol, pivmin);
+}
+
+// * * * * * * * * * * * * * * * Member Functions  * * * * * * * * * * * * * //
+
+void Foam::ICTCSmootherBase::smooth
+(
+     solveScalarField& psi,
+     const scalarField& source,
+     const direction cmpt,
+     const label nSweeps
+) const
+{
+    const solveScalar* __restrict__ diagL = diagL_.begin();
+    const solveScalar* __restrict__ lowerL = lowerL_.begin();
+    const label* __restrict__ rowAddrL = rowAddrL_.begin();
+    const label* __restrict__ colPtrL = colPtrL_.begin();
+
+    solveScalarField rA(diagL_.size());
+
+    solveScalar* __restrict__ rAPtr = rA.begin();
+
+    const label n = rA.size();
+
+    for (label sweeps = 0; sweeps < nSweeps; sweeps++) {
+
+	matrix_.residual
+         (
+             rA,
+             psi,
+             source,
+             interfaceBouCoeffs_,
+             interfaces_,
+             cmpt
+         );
+
+    	// --- Forward solve with L
+    	for (label i = 0; i < n; i++) {
+
+            rAPtr[i] *= diagL[i];
+            scalar s = rAPtr[i];
+            for (label k = colPtrL[i]; k < colPtrL[i+1]; k++) {
+           	rAPtr[rowAddrL[k]] -= lowerL[k] * s;
+            }
+    	}
+
+    	// --- Backward solve with L^T
+    	for (label i = n-1; i >= 0; i--) {
+            scalar s = rAPtr[i];
+            for (label k = colPtrL[i]; k < colPtrL[i+1]; k++) {
+            	s -= lowerL[k] * rAPtr[rowAddrL[k]];
+            }
+
+            rAPtr[i] = s * diagL[i];
+    	}
+
+	psi -= rA; 
+
+    }
+
+}
+
+void Foam::ICTCSmootherBase::scalarSmooth
+(
+     solveScalarField& psi,
+     const solveScalarField& source,
+     const direction cmpt,
+     const label nSweeps
+) const
+{
+    smooth
+    (
+        psi,
+        ConstPrecisionAdaptor<scalar, solveScalar>(source),
+        cmpt,
+        nSweeps
+    );
+
+}
+
+// ************************************************************************* //
